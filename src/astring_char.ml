35601ae74e4947ccf6efb9ff217dd69769502d1a
Daniel Bünzli <daniel.buenzli@erratique.ch>
2020-06-18 15:27:58 +0200

    Remove some watermarks.

--- src/astring_char.ml
+++ src/astring_char.ml
@@ -1,7 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2015 The astring programmers. All rights reserved.
    Distributed under the ISC license, see terms at the end of the file.
-   %%NAME%% %%VERSION%%
   ---------------------------------------------------------------------------*)
 
 let err_byte b = Printf.sprintf "%d is not a byte" b
